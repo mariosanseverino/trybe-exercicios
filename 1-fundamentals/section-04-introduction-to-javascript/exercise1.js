1f5233c45876bd9f3bc3c6159ec30c9d5854c536
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2022-10-20 18:12:42 -0300

    First three exercises proposed

---
+++ 1-fundamentals/section-04-introduction-to-javascript/exercise1.js
@@ -0,0 +1,4 @@
+const a = 6, b = 66
+let add = a + b, sub = a - b, mul = a * b, div = a / b, mod = a % b
+
+console.log(add, sub, mul, div, mod)
---
+++ 1-fundamentals/section-04-introduction-to-javascript/exercise2.js
@@ -0,0 +1,7 @@
+const a = 6, b = 16
+
+if (a > b) {
+    console.log(`A(${a}) is bigger than B(${b}).`)
+} else {
+    console.log(`B(${b}) is bigger than A(${a}).`)
+}

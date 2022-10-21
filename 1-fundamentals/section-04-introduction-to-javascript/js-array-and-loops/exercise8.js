702da750304836f8773e6b5c676874bff348a262
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2022-10-21 18:02:22 -0300

    Exercises 8 and 9.

---
+++ 1-fundamentals/section-04-introduction-to-javascript/js-array-and-loops/exercise8.js
@@ -0,0 +1,7 @@
+let array = []
+
+for (let i = 0; i < 25; i++) {
+    array.push(i+1)
+}
+
+console.log(array)
---
+++ 1-fundamentals/section-04-introduction-to-javascript/js-array-and-loops/exercise9.js
@@ -0,0 +1,11 @@
+let array = []
+
+for (let i = 0; i < 25; i++) {
+    array.push(i+1)
+}
+
+console.log(array)
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]/2)
+}
